5ab68ba40ffbc2155da9c9ab8197d6f37a734262
Al-Evst <al.evst96@gmail.com>
2025-10-08 18:58:58 +0200

    setup DialogFlow

--- bot.py
+++ bot.py
@@ -1,18 +1,36 @@
 from telegram import Update
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, CallbackContext
+from google.cloud import dialogflow
+from google.protobuf.json_format import MessageToDict
 from dotenv import load_dotenv
 import os
 
 
 load_dotenv()
 
+project_id = os.getenv('DIALOGFLOW_PROJECT_ID')
+language_code = 'ru-RU'
+
+
+def detect_intent_text(project_id, session_id, text, language_code):
+    session_client = dialogflow.SessionsClient()
+    session = session_client.session_path(project_id, session_id)
+    text_input = dialogflow.TextInput(text=text, language_code=language_code)
+    query_input = dialogflow.QueryInput(text=text_input)
+    response = session_client.detect_intent(request={"session": session, "query_input": query_input})
+    return MessageToDict(response._pb)
+
 
 def start(update: Update, context: CallbackContext) -> None:
-    update.message.reply_text('Здравствуйте')
+    session_id = str(update.effective_user.id)
+    response = detect_intent_text(project_id, session_id, "привет", language_code)
+    update.message.reply_text(response['queryResult']['fulfillmentText'])
 
 
 def echo(update: Update, context: CallbackContext) -> None:
-    update.message.reply_text(update.message.text)
+    session_id = str(update.effective_user.id)
+    response = detect_intent_text(project_id, session_id, update.message.text, language_code)
+    update.message.reply_text(response['queryResult']['fulfillmentText'])
 
 
 def main() -> None:
